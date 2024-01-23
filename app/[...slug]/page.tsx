815afd878bff5405156481054cf54e0f9a86ce1e
Kinda Hall <1142704468@qq.com>
2024-01-23 20:45:50 +0800

    feat: 🎸 完善description样式

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -98,6 +98,8 @@ const Page = async ({ params }: PageProps) => {
 					{ 'lg:prose-lg': page.style !== 'website' },
 					{ 'max-w-none': page.style === 'website' }
 				)}>
+				<div>{page.description}</div>
+				<hr />
 				<Mdx code={page.body.code}></Mdx>
 				<Comments />
 			</article>
