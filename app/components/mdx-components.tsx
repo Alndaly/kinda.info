4645c3c95c10d9dae7540b87d73963ebd8d7285f
Kinda Hall <1142704468@qq.com>
2024-02-12 20:30:28 +0800

    feat: 🎸 完善关于页面

--- app/components/mdx-components.tsx
+++ app/components/mdx-components.tsx
@@ -2,13 +2,15 @@ import Image from 'next/image';
 import type { MDXComponents } from 'mdx/types';
 import BiliBili from './bilibili';
 import Youtube from './youtube';
+import MyInfo from './my-info';
 import { useMDXComponent } from 'next-contentlayer/hooks';
 
 // Define your custom MDX components.
 const mdxComponents: MDXComponents = {
 	Image,
 	BiliBili,
-	Youtube
+	Youtube,
+	MyInfo
 };
 
 interface MdxProps {
---
+++ app/components/my-info.tsx
@@ -0,0 +1,42 @@
+'use client';
+import { allPosts } from 'contentlayer/generated';
+import readingTime from 'reading-time';
+import { useEffect, useState } from 'react';
+
+const MyInfo = () => {
+	const [words, setWords] = useState<number>(0);
+	useEffect(() => {
+		setWords(
+			allPosts
+				.map((item) => readingTime(item.body.raw).words)
+				.reduce((prev, current) => {
+					return prev + current;
+				})
+		);
+	}, []);
+	return (
+		<div className='w-full p-5 flex justify-center items-center flex-col gap-3'>
+			<div
+				className='rounded-full bg-center no-repeat bg-cover w-24 h-24'
+				style={{
+					backgroundImage: `url(https://oss.kinda.info/image/202402122029799.jpeg)`,
+				}}></div>
+			<div>阡陌</div>
+			<div className='flex flex-row gap-5'>
+				<div className='flex flex-col items-center justify-center'>
+					<div>Posts</div>
+					<div className='text-slate-500 dark:text-slate-400'>
+						{allPosts.length}
+					</div>
+				</div>
+				<div className='w-px dark:bg-white/20 bg-black/20'></div>
+				<div>
+					<div>Words</div>
+					<div className='text-slate-500 dark:text-slate-400'>{words}</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default MyInfo;
