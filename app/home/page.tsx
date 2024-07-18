57730836423c9343b28d258ec1b0bef58ed7c599
Kinda Hall <1142704468@qq.com>
2024-07-18 14:49:29 +0800

    feat: 🎸 完善首页自媒体类型

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,7 +2,9 @@
 /* eslint-disable @next/next/no-img-element */
 import { BiliBiliIcon } from '@/components/icon/bilibili-icon';
 import { GithubIcon } from '@/components/icon/github-icon';
+import { WxOfficialAccountIcon } from '@/components/icon/wx-official-account';
 import { XiaoHongShuIcon } from '@/components/icon/xiaohongshu-icon';
+import { YoutubeIcon } from '@/components/icon/youtube-icon';
 
 export default function Home() {
 	return (
@@ -13,7 +15,7 @@ export default function Home() {
 				alt='my avatar'
 			/>
 			<div className='font-bold text-lg mt-4 font-serif'>Hello, I'm Kinda</div>
-			<div className='mt-4 flex flex-row gap-4'>
+			<div className='mt-4 flex flex-row gap-4 rounded-full backdrop-blur-md bg-black/20 dark:bg-black/50 p-2'>
 				<div>
 					<BiliBiliIcon />
 				</div>
@@ -22,6 +24,12 @@ export default function Home() {
 				</div>
 				<div>
 					<GithubIcon />
+				</div>
+				<div>
+					<YoutubeIcon />
+				</div>
+				<div>
+					<WxOfficialAccountIcon />
 				</div>
 			</div>
 		</div>
