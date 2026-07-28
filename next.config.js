0dfdb942d15d3f6dba6bd7019a29da1db350b63c
Kinda Hall <1142704468@qq.com>
2026-07-28 22:53:15 +0800

    feat: 🎸 Add some docs

--- next.config.js
+++ next.config.js
@@ -15,6 +15,11 @@ const nextConfig = {
         hostname: 'alnda-public.oss-cn-hangzhou.aliyuncs.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'qingyon-revornix-public.oss-cn-beijing.aliyuncs.com',
+        pathname: '/**',
+      },
     ],
   },
 };
